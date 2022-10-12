a5f61a411bd166516bc80ee7dd7088d684c6560e
Daniel Culliver <daniel.culliver@gmail.com>
2022-10-12 15:49:42 +0200

    dfgdf

--- CopiedClasses/CopiedClasses/Program.cs
+++ CopiedClasses/CopiedClasses/Program.cs
@@ -28,21 +28,24 @@ namespace CopiedClasses
     class Premier
     {
         public string jmeno;
+        public Vlada vlada;
+
         public string SilneProhlaseni()
         {
-            return "Nevim o cem to mluvite.";
+            return "Premier " + jmeno + " říká: Nevim o cem to mluvite.";
         }
-        public Vlada vlada;
     }
 
     class Ministr
     {
         public string jmeno;
+        public Premier premier;
+
         public string SlabeProhlaseni()
         {
-            return "Ja nevim co bych na to rekl, ale pan premier by na to rekl: " + premier.SilneProhlaseni();
+            return jmeno + " říká: Ja nevim co bych na to rekl, ale pan premier by na to rekl: " + premier.SilneProhlaseni();
         }
-        public Premier premier;
+
         private void Demise()
         {
             int index = -1;
@@ -60,27 +63,44 @@ namespace CopiedClasses
         }
     }
 
+    class Hacker
+    {
+        public void InfiltrateGov(Vlada Gov)
+        {
+            Gov.premier = new Premier();
+            Gov.premier.jmeno = "Ondrej";
+            Gov.premier.vlada = Gov;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             Vlada Government = new Vlada();
             Premier PrimeMinister = new Premier();
-            List<Ministr> Ministers = new List<Ministr>() { new Ministr(), new Ministr(), new Ministr(), new Ministr(), new Ministr(), new Ministr() };
+            List<Ministr> Ministers = new List<Ministr>() {};
 
             Government.zeme = "Česko";
             Government.premier = PrimeMinister;
             Government.ministri = Ministers;
+
             PrimeMinister.jmeno = "Ondřej";
             PrimeMinister.vlada = Government;
 
-            for (int i = 0; i < Ministers.Count; i++)
+            int ministerAmount = 10;
+
+            for (int i = 0; i < ministerAmount; i++)
             {
-                Ministers[i] = new Ministr();
-                Ministers[i].jmeno = "guy" + i.ToString();
+                Ministers.Add(new Ministr());
+                Ministers[i].jmeno = "Minister_" + i.ToString();
                 Ministers[i].premier = PrimeMinister;
             }
 
+            Console.WriteLine(Government.premier.SilneProhlaseni());
+            Console.WriteLine(Government.ministri[0].SlabeProhlaseni());
+
+            Console.Read();
         }
     }
 }
