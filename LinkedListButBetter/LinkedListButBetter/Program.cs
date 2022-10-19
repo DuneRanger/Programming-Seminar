916937e293327ed2cc3be93b8c22bed39af067e7
Daniel Culliver <daniel.culliver@gmail.com>
2022-10-19 15:56:15 +0200

    better better linked list

--- LinkedListButBetter/LinkedListButBetter/Program.cs
+++ LinkedListButBetter/LinkedListButBetter/Program.cs
@@ -32,6 +32,20 @@ namespace LinkedListButBetter
                 return curNode;
             }
         }
+        public int length
+        {
+            get
+            {
+                Node curNode = startNode;
+                int index = 0;
+                while (curNode.next != null)
+                {
+                    curNode = curNode.next;
+                    index++;
+                }
+                return index+1;
+            }
+        }
 
         public LinkedList(Node initialNode)
         {
@@ -41,7 +55,6 @@ namespace LinkedListButBetter
         public void addNodeToEnd(int newValue)
         {
             lastNode.next = new Node(newValue);
-            return;
         }
 
         public void addNodeToStart(int newValue)
@@ -49,7 +62,6 @@ namespace LinkedListButBetter
             Node newStart = new Node(newValue);
             newStart.next = startNode;
             startNode = newStart;
-            return;
         }
 
         public Node returnFirst()
@@ -62,6 +74,40 @@ namespace LinkedListButBetter
             startNode = startNode.next;
         }
 
+
+        public void addNodeDynamically(int newValue, int index)
+        {
+            if (index < length)
+            {
+                int curInd = 0;
+                Node curNode = startNode;
+                while(curInd < index-1)
+                {
+                    curNode = curNode.next;
+                    curInd++;
+                }
+                Node newNode = new Node(newValue);
+                newNode.next = curNode.next;
+                curNode.next = newNode;
+            }
+        }
+
+        public dynamic returnNodeDynamically(int index)
+        {
+            if (index < length)
+            {
+                int curInd = 0;
+                Node curNode = startNode;
+                while (curInd < index)
+                {
+                    curNode = curNode.next;
+                    curInd++;
+                }
+                return curNode;
+            }
+            return null;
+        }
+
         public void writeList()
         {
             Node curNode = startNode;
@@ -72,44 +118,34 @@ namespace LinkedListButBetter
                 curNode = curNode.next;
             }
             Console.Write(curNode.value + "]");
-            return;
-        }
-
-        public void
-        
+            Console.WriteLine();
+        }        
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            LinkedList testing = new LinkedList(new Node(1));
+            testing.addNodeToEnd(2);
+            testing.addNodeToEnd(3);
+            testing.addNodeToEnd(4);
+            testing.addNodeToEnd(5);
+            testing.writeList();
+
+            testing.removeFirst();
+            testing.writeList();
+
+            testing.addNodeToStart(9);
+            Console.WriteLine(testing.returnFirst().value.ToString());
+            testing.writeList();
+
+            testing.addNodeDynamically(8, 3);
+            testing.writeList();
+
+            Console.WriteLine(testing.returnNodeDynamically(3).value.ToString());
 
             Console.ReadLine();
         }
     }
-
-    //    private void add(Node newNode, int depth = -1)
-    //    {
-    //        Node currentNode = start;
-    //        if (depth == -1)
-    //        {
-    //            getLast().next = newNode;
-    //            return;
-    //        }
-    //        while(depth > 0)
-    //        {
-    //            currentNode = currentNode.next;
-    //            depth--;
-    //        }
-    //        Node nextNode = currentNode.next;
-    //        currentNode.next = newNode;
-    //        newNode.next = nextNode;
-    //        return;
-    //    }
-
-    //    public LinkedList(Node a)
-    //    {
-    //        start = a;
-    //    }
-    //}
 }
